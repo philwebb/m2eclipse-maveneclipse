f0f537b30df89bc1146b963bcdadf26b558e0e4d
Phillip Webb <pwebb@pivotal.io>
2018-02-07 12:28:14 -0800

    Update publish

--- publish.sh
+++ publish.sh
@@ -5,4 +5,3 @@ curl -uphilwebb:${KEY} -X PUT -T "./publish/target/repository/content.jar" \
   https://api.bintray.com/content/philwebb/m2eclipse-maveneclipse/content.jar || exit 1
 curl -uphilwebb:${KEY} -X PUT -T "./publish/target/repository/artifacts.jar" \
   https://api.bintray.com/content/philwebb/m2eclipse-maveneclipse/artifacts.jar || exit 1
-
